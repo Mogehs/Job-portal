861043c542c93023e37879f2933cdb16cf1e95dd
Mogehs <mugheesurrehman06@gmail.com>
2024-11-02 03:39:59 +0500

    a lol error resolve

--- frontend/src/components/admin/RouteProtected.jsx
+++ frontend/src/components/admin/RouteProtected.jsx
@@ -6,7 +6,7 @@ const RouteProtected = ({ children }) => {
   const { user } = useSelector((store) => store.auth);
   const navigate = useNavigate();
   useEffect(() => {
-    if (user != null && user.role != recruiter) {
+    if (user != null && user.role != "recruiter") {
       navigate("/");
     }
   });
