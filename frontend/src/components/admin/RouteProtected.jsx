5c6c35736f6c4f18af96e2437c3563f0658d23b2
Mogehs <mugheesurrehman06@gmail.com>
2024-11-02 03:48:47 +0500

    again a mistake

--- frontend/src/components/admin/RouteProtected.jsx
+++ frontend/src/components/admin/RouteProtected.jsx
@@ -5,11 +5,15 @@ import { useNavigate } from "react-router-dom";
 const RouteProtected = ({ children }) => {
   const { user } = useSelector((store) => store.auth);
   const navigate = useNavigate();
+
   useEffect(() => {
-    if (user != null && user.role != "recruiter") {
+    if (user && user.role !== "recruiter") {
       navigate("/");
     }
-  });
+  }, [user, navigate]);
+
+  // Conditionally render children if user is a recruiter
+  return user && user.role === "recruiter" ? <>{children}</> : null;
 };
 
 export default RouteProtected;
