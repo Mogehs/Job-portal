9bd0cd79f342c40560286f96c565a12feb0760ef
Mogehs <mugheesurrehman06@gmail.com>
2024-11-02 03:20:08 +0500

    hostchange

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://job-portal-0y94.onrender.com",
   credentials: true,
 };
 
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,8 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const JOBS_API_END_POINT = "http://localhost:8000/api/v1/job";
-export const APP_API_END_POINT = "http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+export const USER_API_END_POINT =
+  "https://job-portal-0y94.onrender.com/api/v1/user";
+export const JOBS_API_END_POINT =
+  "https://job-portal-0y94.onrender.com/api/v1/job";
+export const APP_API_END_POINT =
+  "https://job-portal-0y94.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT =
+  "https://job-portal-0y94.onrender.com/api/v1/company";
